9dfceb2bd0121c6a15b504c2ee4a3fbaacf905bf
yinhuochong <350905402@qq.com>
2017-01-15 22:10:19 +0800

    1

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
 # coding=utf-8
 import subprocess32
-from Queue import Queue
-from threading import Thread
-from threadpool import makeRequests, ThreadPool
-from parseurl import parseurl
+import sys
 import time
 import os
+from Queue import Queue
+from threading import Thread
+from threading import Condition
+from threading import Lock
+from threadpool import makeRequests, ThreadPool
+from parseurl import parseurl
+from setting import mongodb
 from datetime import datetime
 from optparse import OptionParser
-import sys
-from setting import mongodb
 
 #页面爬行类
-class Crawler(object):
+class crawler(object):
     def __init__(self, url, domain, depth, threadNum):
-        #要获取的URL的队列
+        #当前正在执行的线程数
+        self.currentRunning = 0
+        #线程池管理线程与爬虫线程同步的条件变量,用于修改currentRunning时的加锁和唤醒管理线程
+        self.processCondition = Condition()
+        #待访问的URL的队列
         self.urlQueue = Queue()
         #已经访问的URL
         self.readUrls = []
-        #未被访问的URL
-        self.links = []
         #URL host、path、param的键值对
         self.urls = {}
         #线程数
@@ -28,12 +32,13 @@ class Crawler(object):
         #设定了线程数的线程池
         #self.threadPool = ThreadPool(self.threadNum)
         self.pool = ThreadPool(self.threadNum)
+
         #初始化URL队列
-        self.urlQueue.put(url)
-        #爬行深度
+        self.urlQueue.put({'url':url,"depth":1})
+        #预期爬行深度
         self.depth = depth
         #当前爬行深度
-        self.currentDepth = 1
+        #self.currentDepth = 1
         #当前运行状态
         self.state = False
         #DOMAIN
@@ -41,7 +46,7 @@ class Crawler(object):
         #初始化数据库
         self.db = mongodb(self.domain)
         self.db.clean()
-    
+
     #URL字典转字符串
     def dic2url(self, udic):
         url = udic['schema'] + '://' + udic['host'] + udic['path'] + '?'
@@ -110,7 +115,7 @@ class Crawler(object):
         for r in list(set(result.split('\n'))):
             if r != '':
                 urldict = parseurl(r).getParse()
-                if urldict['host'] == self.domain:
+                if self.domain in urldict['host']:
                     if urldict not in temp:
                         temp.append(urldict)
         for x in temp:
@@ -119,68 +124,98 @@ class Crawler(object):
         return urls
 
     #获取当前页面的URL
-    def work(self, url):
-        print '[*] Current url is %s' % url
+    def work(self, urlInfo):
+        url = urlInfo['url']
+        currentDepth = urlInfo['depth']
+        #print '[*] Current url is %s' % url
         try:
             child = subprocess32.Popen(['phantomjs', 'phantomjs.js', url], stdout=subprocess32.PIPE)
             output = child.communicate(None, timeout=10)
             #output = subprocess32.check_output(['phantomjs', 'phantomjs.js', url], timeout=10)
         except Exception, e:
+            os.kill(child.pid,9)
+            self.processCondition.acquire()
+            self.currentRunning = self.currentRunning - 1
+            self.processCondition.notify()
+            self.processCondition.release()
+            print '[!] error in url %s' % url
             return None
+
         urls = self.duplicateFilter(output[0])
         #urls = self.duplicateFilter(output)
-        for i in urls:
-            if i not in self.readUrls:
-                self.db.insert(dict([['link',i.decode("unicode_escape")]]))
-                self.links.append(i)
+        if currentDepth >= self.depth:
+            for i in urls:
+                if i not in self.readUrls:
+                    self.db.insert(dict([['link',i.decode("unicode_escape")]]))
+        else:
+            for i in urls:
+                if i not in self.readUrls:
+                    self.db.insert(dict([['link',i.decode("unicode_escape")]]))
+                    self.urlQueue.put({'url':i,'depth':currentDepth+1})
+
+        self.processCondition.acquire()
+        self.currentRunning = self.currentRunning - 1
+        self.processCondition.notify()
+        self.processCondition.release()
+
+    def urlQueueProcesser(self):
+        self.processCondition.acquire()
+        while True:
+            if not(self.urlQueue.empty() and self.currentRunning == 0):
+                urls = []
+                #收集url队列
+                while not self.urlQueue.empty():
+                    item = self.urlQueue.get()
+                    if item["url"] not in self.readUrls:
+                        urls.append(item)
+                        self.readUrls.append(item["url"])
+                #将队列里的URL都加入到线程池
+                threadNum = len(urls)
+                requests = makeRequests(self.work, urls)
+                self.currentRunning = self.currentRunning + threadNum
+                [self.pool.putRequest(req) for req in requests]
+                self.processCondition.wait()
+            else:
+                break
 
     def start(self):
         self.state = True
         print '\n[-] Starting Crawling...........Domain is %s\n' % self.domain
-        while self.currentDepth <= self.depth:
-            urls = []
-            while not self.urlQueue.empty():
-                url = self.urlQueue.get()
-                urls.append(url)
-                self.readUrls.append(url)
-            #将队列里的URL都加入到线程池
-            requests = makeRequests(self.work, urls)
-            [self.pool.putRequest(req) for req in requests]
-            self.pool.wait()
-            for i in self.links:
-                if i not in self.readUrls:
-                    self.urlQueue.put(i)
-            self.currentDepth += 1
+        self.urlQueueProcesser();
+
         self.stop()
 
     def stop(self):
         self.db.close_db()
         self.state = False
+        #self.pool.wait()
 
 
 
 class printInfo(Thread):
-    def __init__(self, Crawler):
+    def __init__(self, crawler):
         Thread.__init__(self)
         self.startTime = datetime.now()
         self.daemon = True
-        self.Crawler = Crawler
+        self.crawler = crawler
         self.start()
-    
-    def run(self):
-        while True:
-            if self.Crawler.state == True:
-                time.sleep(10)
-                print '[+] CurrentDepth: %d, Totally visited %d links.\n' % (self.Crawler.currentDepth -1, len(self.Crawler.readUrls))
-    
+
     def printEnd(self):
         self.endTime = datetime.now()
-        print 'Crawl Depth:%d, Totally visited %d Links.\n' % (self.Crawler.currentDepth-1, len(self.Crawler.readUrls))
+        print 'Crawl Depth:%d, Totally visited %d links.\n' % (self.crawler.depth, len(self.crawler.readUrls))
         print 'Start at: %s' % self.startTime
         print 'End at: %s' % self.endTime
         print 'Spend time: %s' % (self.endTime - self.startTime)
-        print 'Result count: %s\n' % (self.Crawler.db.count())
+        print 'Result count: %s\n' % (self.crawler.db.count())
         print '[-] Finished......'
+
+    def run(self):
+        while True:
+            if self.crawler.state == True:
+                time.sleep(10)
+                print '[+] Now totally visited %d links , %d threads is in threads queue .\n' % (len(self.crawler.readUrls),self.crawler.currentRunning)
+
+
 
 
 if __name__ == '__main__':
@@ -198,7 +233,7 @@ if __name__ == '__main__':
         print optParser.print_help()
     else:
         domain = parseurl(options.url).getHost()
-        spider = Crawler(options.url, domain, options.depth, options.thread)
+        spider = crawler(options.url, domain, options.depth, options.thread)
         info = printInfo(spider)
         spider.start()
         info.printEnd()
